2aa225fa4333db66e6cc1dece429bd83a150040d
Muhammed Ashique <mashiksmailbox@gmail.com>
2025-04-10 22:32:20 +0530

    redis connection testing

--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -1,12 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { InjectQueue } from '@nestjs/bull';
 import { Queue } from 'bull';
+import Redis from 'ioredis';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class NotificationsService {
-  constructor(@InjectQueue('habit-reminders') private reminderQueue: Queue) {}
+  constructor(
+    @InjectQueue('habit-reminders') private reminderQueue: Queue,
+    private configService: ConfigService,
+  ) {}
 
   async scheduleReminder(userId: number, habitId: number, time: string) {
+    const redis = new Redis(
+      this.configService.get<string>('UPSTASH_REDIS_URL')!,
+      {
+        tls: {},
+      },
+    );
+    redis
+      .set('test23', 'success')
+      .then(() => redis.get('test'))
+      .then(() => {
+        console.log('Redis connection successful');
+      })
+      .catch(() => {
+        console.error('Redis connection failed');
+      });
     const reminderTime = new Date(time);
     await this.reminderQueue.add(
       'send-reminder',
