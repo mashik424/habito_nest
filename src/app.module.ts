122668ef35a0a4e348c01067b84dc85e66ae9b5a
Muhammed Ashique <mashiksmailbox@gmail.com>
2025-04-10 14:54:34 +0530

    disabled postrgres synchronize

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,7 +25,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         type: 'postgres',
         url: configService.get<string>('POSTGRES_URL'),
         autoLoadEntities: true,
-        synchronize: true, // disable in prod and use migrations
+        synchronize: false, // disable in prod and use migrations
         entities: [User, Habit],
         ssl: {
           rejectUnauthorized: false,
